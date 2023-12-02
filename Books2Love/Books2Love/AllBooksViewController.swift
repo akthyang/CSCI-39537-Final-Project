a47a02bc2c5167b2c8394e78bdb370bf07bc3314
Alicia Yang <akthyang@gmail.com>
2023-12-02 14:01:38 -0500

    added the page to display all pages

--- Books2Love/Books2Love/AllBooksViewController.swift
+++ Books2Love/Books2Love/AllBooksViewController.swift
@@ -2,28 +2,76 @@
 //  AllBooksViewController.swift
 //  Books2Love
 //
-//  Created by Christine Yang on 12/2/23.
-//
+//  Created by Alicia Yang on 12/2/23.
+//  Displays a list of books and lightnovels
 
 import UIKit
 
 class AllBooksViewController: UIViewController {
 
+    let bookCell = "bookCell"
+    var books = [Book]()
+    
+    lazy var AllBooksTableView: UITableView = {
+        let AllBooksTableView = UITableView()
+        AllBooksTableView.dataSource = self
+        AllBooksTableView.delegate = self
+        AllBooksTableView.register(BooksTableViewCell.self, forCellReuseIdentifier: bookCell)
+        return AllBooksTableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.addSubview(AllBooksTableView)
+        
+        // calls the GoogleBook API
+        Task {
+            do {
+                let books = try await BooksAPI.shared.popularBooksWest()
+                self.books = books
+                AllBooksTableView.reloadData()
+            }
+            catch {
+                print(error)
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: makes sure table remains in the frame of the phone
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        AllBooksTableView.frame = view.bounds
     }
-    */
+}
+
+
+// MARK: TableView Delegate and Data Source
+extension AllBooksViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: Code for each cell
+    
+    func tableView(_ AllBooksTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ AllBooksTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = AllBooksTableView.dequeueReusableCell(withIdentifier: bookCell, for: indexPath) as! BooksTableViewCell
+        var book = books[indexPath.row]
+        if let safeURL = URL(string: book.volumeInfo.imageLinks.thumbnail) {
+            cell.thumbnail.loadImage(url: safeURL)
+        }
+        else {
+            cell.thumbnail.image = UIImage(imageLiteralResourceName: "Noimage")
+        }
+        cell.bookTitle.text = book.volumeInfo.title
+        cell.descript.text = book.searchInfo.textSnippet
+        return cell
+    }
+    
+    func tableView(_ AllBooksTableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 350
+    }
 
 }

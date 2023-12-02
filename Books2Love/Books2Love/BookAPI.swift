76c186498c13bc94d8d0185e46fc3d0a2f766098
Alicia Yang <akthyang@gmail.com>
2023-12-02 13:45:44 -0500

    added the endpoints and apicall to Google Books

--- Books2Love/Books2Love/BookAPI.swift
+++ Books2Love/Books2Love/BookAPI.swift
@@ -3,6 +3,69 @@
 //  Books2Love
 //
 //  Created by Alicia Yang on 12/2/23.
-//
+//  Calls the Google Books API and returns books and lightnovels
 
 import Foundation
+
+struct BooksAPI {
+        
+    static let shared = BooksAPI()
+    
+    private let urlSession: URLSession
+    
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+    
+    // MARK: gets a list of popular Young Adult Books from Google Books
+    func popularBooksWest() async throws -> [Book] {
+        let url = URL(string: "https://www.googleapis.com/books/v1/volumes?q=most+popular+young+adult+novels+2023&key=AIzaSyDDIitdJ5Puu3-W0mchd8hVbIeGHzPzHxQ")!
+        let urlRequest = URLRequest(url: url)
+        let (data, _) = try await urlSession.data(for: urlRequest)
+        
+        let items = try JSONDecoder().decode(GoogleBookResponse.self, from: data)
+        let bookInfo = items.items
+        return bookInfo
+    }
+    
+    // MARK: gets 10 lightnovels from Google Books
+    func lightNovels() async throws -> [Book] {
+        let url = URL(string: "https://www.googleapis.com/books/v1/volumes?q=best+light+novels&key=AIzaSyDDIitdJ5Puu3-W0mchd8hVbIeGHzPzHxQ")!
+        let urlRequest = URLRequest(url: url)
+        let (data, _) = try await urlSession.data(for: urlRequest)
+        
+        let items = try JSONDecoder().decode(GoogleBookResponse.self, from: data)
+        let bookInfo = items.items
+        return bookInfo
+    }
+    
+}
+
+// format to decode the API response from Google Book's  API
+// Adapted from: https://stackoverflow.com/questions/70645037/no-value-associated-with-key-codingkeys-error
+struct Book: Codable {
+    let volumeInfo: VolumeInfo
+    let searchInfo: SearchInfo
+}
+
+struct VolumeInfo: Codable {
+    let title: String
+    let authors: [String]?
+    let description: String
+    let categories: [String]?
+    let imageLinks: ImageLinks
+}
+
+struct SearchInfo: Codable {
+    let textSnippet: String
+}
+
+struct ImageLinks: Codable {
+    let thumbnail: String
+}
+
+struct GoogleBookResponse: Codable {
+    let kind: String
+    let items: [Book]
+}
+

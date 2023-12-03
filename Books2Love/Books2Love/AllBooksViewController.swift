ab8010418dbd7eafa073efad1eee95ce9a2ea059
Alicia Yang <akthyang@gmail.com>
2023-12-03 15:09:47 -0500

    fixed warning and update api

--- Books2Love/Books2Love/AllBooksViewController.swift
+++ Books2Love/Books2Love/AllBooksViewController.swift
@@ -58,7 +58,7 @@ extension AllBooksViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ AllBooksTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = AllBooksTableView.dequeueReusableCell(withIdentifier: bookCell, for: indexPath) as! BooksTableViewCell
-        var book = books[indexPath.row]
+        let book = books[indexPath.row]
         if let safeURL = URL(string: book.volumeInfo.imageLinks.thumbnail) {
             cell.thumbnail.loadImage(url: safeURL)
         }
